c43fd2686d9e322caee809ddf617a571c73a017d
Wolfgang Kleinbach <register@zeichensatz.de>
2021-10-12 15:23:03 +0200

    Added directory

---
+++ Tests/Functional/BasicTest.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace Wok\WokGpxviewer\Tests\Functional;
+
+use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
+
+/**
+ * Test case
+ */
+class BasicTest extends FunctionalTestCase
+{
+    /**
+     * @var array
+     */
+    protected $testExtensionsToLoad = [
+        'typo3conf/ext/wok_gpxviewer',
+    ];
+
+    /**
+     * Just a dummy to show that at least one test is actually executed
+     *
+     * @test
+     */
+    public function dummy()
+    {
+        $this->assertTrue(true);
+    }
+}
